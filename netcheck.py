ba81f4d068857947a06cf04cf910ac266833edbc
Zedenar <36276054+Zedenar@users.noreply.github.com>
2024-03-12 14:43:50 +0100

    Update netcheck.py

--- netcheck.py
+++ netcheck.py
@@ -1,6 +1,6 @@
 import nmap, time, os
 
-# Please add the IP's you want to "white-list" e.g. the IP's that SHOULD have access to your network.
+# Please add the IP's you want to "whitelist" e.g. the IP's that SHOULD have access to your network.
 # Any unknown IP's will get flagged.
 iplist = [
         '192.168.1.1',  # Example
@@ -8,30 +8,42 @@ iplist = [
         '192.168.1.3',  # Example
         ]
 
-# Create a new instance of the PortScanner class.
+# Create a new instance of the PortScanner class
 nm = nmap.PortScanner()
+unknownlist = []
 
-# Check if the found IP is whitelisted or not.
+def action():
+    for intruder in unknownlist:
+        print("Slapping intruder !!!") # Template
+
+def unknownlistappend():
+    if host not in unknownlist:
+        unknownlist.append(host)
+
 def check():
     if host not in iplist:
-        return("<---- Possible Intruder")
+        unknownlistappend()
+        return("<---- 'Unknown IP Found!")
     else:
-        pass
+        return(" ")
 
 while True:
-    nm.scan(hosts='192.168.2.0/24', arguments='-sn -A -sP -PE') #  Make sure to change " hosts='<change this>' ", 
-                                                                #  and perhaps also the arguments if you wish to do so.
-    try:                                                        #  https://pypi.org/project/python-nmap/
-        os.system('clear')  # Linux/Unix 
+    nm.scan(hosts='192.168.2.0/24', arguments='-sn -A -sP -PE')  #  Make sure to change " hosts='<change this>' ", 
+                                                                 #  and perhaps also the arguments if you wish to do so.
+	                                                         #  https://pypi.org/project/python-nmap/
+    try:
+        os.system('clear')
     except:
-        os.system('cls')  # Windows
+        os.system('cls')
         
     print(5*"-", "Active IP's in the Network", 5*"-", "Ctrl+c to quit")
-
-    # Print out all the found IP's and flag every IP which is not included in the white-list.
+    
     for host in nm.all_hosts():
         if nm[host].state() == 'up':
-            print('Host : %s (%s)' % (host, nm[host].hostname()),check())
+            print('Host : %s (%s)' % (host, nm[host].hostname()), check())
+    print(5*"-", "Unknown IP's", 5*"-")
+    print(unknownlist)
+    action()
     time.sleep(1)
     
         
